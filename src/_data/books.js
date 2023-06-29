98afa372ea23ce11d98acbd87f28acc4b4630d2b
Eric Wallace <ewallace@collin.local>
2023-06-29 16:42:54 -0500

    update 0.1.2 to books

--- src/_data/books.js
+++ src/_data/books.js
@@ -2,7 +2,7 @@
 
 require('dotenv').config();
 const Airtable = require('airtable');
-const base = new Airtable({ apiKey: process.env.AIRTABLE }).base( process.env.AIRTABLE_BASE_URL);
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base( process.env.AIRTABLE_BASE_ID);
 
 module.exports = async () => {
     try {

87b37c39e78f586936d9e34e56f60ffdf26bd830
Eric Wallace <ewallace@collin.local>
2024-02-16 18:10:52 -0600

    added my-footer navigation

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -34,5 +34,58 @@ module.exports = {
         text: 'Sitemap',
         url: '/sitemap/'
       }
+    ],
+    explore: [
+      {
+        text: 'Blog',
+        url: '/blog/'
+      },   {
+        text: 'Notes',
+        url: '/notes/'
+      }
+    ],
+    about: [
+      {
+        text: 'Colophon',
+        url: '/colophon/'
+      },
+      {
+        text: 'Eric',
+        url: '/eric/'
+      },
+      {
+        text: 'Links',
+        url: '/links/'
+      },
+      {
+        text: 'Now',
+        url: '/now/'
+      },
+      {
+        text: 'Uses',
+        url: '/uses/'
+      },
+      {
+        text: 'Contact',
+        url: '/contact/'
+      },
+      {
+        text: 'Support',
+        url: '/support/'
+      },
+    ],
+    resources: [
+      {
+        text: 'Good News',
+        url: '/good-news/'
+      },
+      {
+        text: 'Preferences',
+        url: '/preferences/'
+    },
+      {
+        text: 'Exits',
+        url: '/exits/'
+      }
     ]
   };

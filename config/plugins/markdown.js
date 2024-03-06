875a48f94a7e801f776c9de5d70d88e138e57c61
Eric Wallace <eric@ecrosstexas.com>
2024-03-05 20:22:29 -0600

    add caption for markdown images

--- config/plugins/markdown.js
+++ config/plugins/markdown.js
@@ -9,6 +9,7 @@ const markdownItFootnote = require('markdown-it-footnote');
 const markdownitMark = require('markdown-it-mark');
 const markdownitAbbr = require('markdown-it-abbr');
 const {slugifyString} = require('../utils');
+const path = require('path');
 
 const markdownLib = markdownIt({
   html: true,
@@ -45,7 +46,7 @@ const markdownLib = markdownIt({
   .use(markdownItEmoji)
   .use(markdownItEleventyImg, {
     imgOptions: {
-      widths: [440, 880],
+      widths: [440, 880, 1024],
       urlPath: '/assets/images/',
       outputDir: './dist/assets/images/',
       formats: ['webp', 'jpeg']
@@ -55,8 +56,29 @@ const markdownLib = markdownIt({
       decoding: 'async',
       sizes: '90vw'
     },
+    // prepend src for markdown images
     resolvePath: (filepath, env) => {
       return path.join('src', filepath);
+    },
+    renderImage(image, attributes) {
+      const [Image, options] = image;
+      const [src, attrs] = attributes;
+
+      Image(src, options);
+
+      const metadata = Image.statsSync(src, options);
+      const imageMarkup = Image.generateHTML(metadata, attrs, {
+        whitespaceMode: 'inline'
+      });
+
+      const imageElement = attrs.title
+        ? `<figure class="flow">
+			${imageMarkup}
+					<figcaption>${attrs.title}</figcaption>
+				</figure>`
+        : `${imageMarkup}`;
+
+      return imageElement;
     }
   })
   .use(markdownItFootnote)
